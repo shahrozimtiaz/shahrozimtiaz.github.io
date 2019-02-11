7c1a35c1cc6c7112e7d75b00a8ecdea468bc28c4
Shahroz Imtiaz <shahrozimtiaz07@gmail.com>
2019-02-11 16:20:47 -0500

    up

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-var ps = prompt("What is the password?");while(true){if(ps.toLowerCase() == "p1"){break;}else{ps = prompt("Wrong. What is the password?");}}
+var ps = prompt("What is the password?");
+while(true){
+	if(ps.toLowerCase() == "p1"){
+		break;
+	}else{ps = prompt("Wrong. What is the password?");}
+}
