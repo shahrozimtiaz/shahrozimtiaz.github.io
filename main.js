672a6086e80bd70cd1820aa03aff7cd5d52c1ecf
Shahroz Imtiaz <shahrozimtiaz07@gmail.com>
2019-04-03 02:51:53 -0400

    up

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+String.prototype.hashCode = function(){
+	var hash = 0;
+	for (var i = 0; i < this.length; i++) {
+		var character = this.charCodeAt(i);
+		hash = ((hash<<5)-hash)+character;
+		hash = hash & hash; // Convert to 32bit integer
+	}
+	return hash;
+}
 var ps = prompt("What is the password?");
 while(true){
-	if(ps.toLowerCase() == "p1"){
-		break;
-	}else{ps = prompt("Wrong. What is the password?");}
+	if(ps == null){ps = prompt("Wrong. What is the password?");}
+	else if(ps.toLowerCase().hashCode() == 3521){break;}
+	else{ps = prompt("Wrong. What is the password?");}
 }
